86e8c9820205faf85ed14f9cd349458c5298116e
C11NJXX <c_11njxxsmailbox@qq.com>
2023-05-04 13:20:15 +0800

    puzzleGame(unfinished)

---
+++ puzzleGame/src/c11njxx/learn/puzzlegame/ui/RegisterJFrame.java
@@ -0,0 +1,17 @@
+package c11njxx.learn.puzzlegame.ui;
+
+import javax.swing.*;
+
+public class RegisterJFrame extends JFrame {
+    public RegisterJFrame() {
+        this.setSize(488,500);
+        //设置窗口的标题
+        this.setTitle("Register for game v1.0");
+        //设置窗口置顶
+        this.setAlwaysOnTop(true);
+        //设置关闭模式
+        this.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+
+        this.setVisible(true);
+    }
+}
